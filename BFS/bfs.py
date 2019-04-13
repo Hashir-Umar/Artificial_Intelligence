4ca0017689ea78a80a4289a7a28a2e78b1649efe
Hashir Umar <32617270+Hashir-Umar@users.noreply.github.com>
2019-04-14 03:07:40 +0500

    final commit

--- BFS/bfs.py
+++ BFS/bfs.py
@@ -3,41 +3,55 @@ actualPath = []
 exploredPath = []
 
 def expand(tree, node):
-
+	
+	#Do not expand leaf node 
 	if node not in tree:
 		exploredPath.append(node)
+		actualPath.pop(0)
 		return
 
-	list = tree[node]
-	list.sort()
-	for i in list:
-		queue.append(i)
-
-	print("Actual Path: ", actualPath)
-
-	#for i in range(0, len(list)):
-		#temp = actualPath[i] + queue[i]
-		#actualPath.append(temp)
-
-
+	chainNode = actualPath.pop(0)
+	chainNodeChild = tree[node]
+	chainNodeChild.sort()
+	
+	for child in chainNodeChild:
+		queue.append(child)
+		actualPath.append(chainNode+child)
+	
 	exploredPath.append(node)
 	
 def algo(tree, node, queue, rNode, gNode):	
 	queue.append(rNode)
+	actualPath.append(rNode)
 	while len(queue) != 0:
 		dequeuedNode = queue.pop(0)
 		if dequeuedNode  == gNode:
 			print("Goal has found")
 			print("Traversed Path: ", exploredPath)
-			print("Actual Path: ", actualPath)
+			print("Actual Path: ", actualPath[0])
 			return
 		if dequeuedNode not in exploredPath:
-			expand(tree, dequeuedNode)	
+			expand(tree, dequeuedNode)
+		else:
+			actualPath.pop(0)
 	print("Goal does not exist")
-	
-rootNode = 'A'
-goalNode = 'F'	
-tree = {'A':['B', 'C'], 'B':['D', 'A'], 'C':['E', 'F']}
+		
+inputFile = open("test.txt","r")
+fileText = inputFile.read().splitlines()
+inputFile.close()
+
+rootNode = fileText[0][0]
+goalNode = fileText[0][2]
+
+tree = {}
+fileText.pop(0)
+for i in fileText:
+	temp = i.split(':')
+	tree[temp[0]] = temp[1].split(',')
+
+print("Root Node: ", rootNode)
+print("Goal Node: ", goalNode)
+print("Tree: ", tree)
 
 algo(tree, rootNode, queue, rootNode, goalNode)
 
